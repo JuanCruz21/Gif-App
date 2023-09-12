7e9f94aacf915a41371a29dfe3b4e7cd41a572f0
JuanCruz21 <juancruz.02.2001@gmail.com>
2023-09-12 06:49:07 -0500

    Nueva funcionalidad

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react"
 
 
 export const GifApp = () => {
+  const [categorias, setCategorias] = useState(['One Punch', 'Drama'])
+  console.log(categorias)
+
+  const onAddCategory = ()=>{
+    setCategorias(categorias => categorias.concat("dato"))
+  }
+
   return (
     <>
     <h1>Gif App</h1>
-    <input type="text" placeholder="Buscar"/>
+    <input type="text" placeholder="Buscar gif"/>
+    <button onClick={onAddCategory}>Agregar</button>
+    <ol>
+      {categorias.map(category =>{
+        return <li key={category}>{category}</li>
+      })}
+    </ol>
     </>
   )
 }
